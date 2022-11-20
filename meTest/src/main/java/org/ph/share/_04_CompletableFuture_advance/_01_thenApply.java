5081659d48d1e21da38e64aee2a2a3db07187943
wolifyy <wolify@qq.com>
2022-11-20 22:10:33 +0800

    apply异步回调

--- meTest/src/main/java/org/ph/share/_04_CompletableFuture_advance/_01_thenApply.java
+++ meTest/src/main/java/org/ph/share/_04_CompletableFuture_advance/_01_thenApply.java
@@ -1,25 +1,34 @@
 package org.ph.share._04_CompletableFuture_advance;
 
-import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.*;
 
 public class _01_thenApply {
     public static void main(String[] args) {
+        ForkJoinPool pool=new ForkJoinPool();
+        ForkJoinPool pool2=new ForkJoinPool();
+        ExecutorService executor = Executors.newFixedThreadPool(2);
         SmallTool.printTimeAndThread("小白吃好了");
         SmallTool.printTimeAndThread("小白 结账、要求开发票");
+
 
         CompletableFuture<String> invoice = CompletableFuture.supplyAsync(() -> {
             SmallTool.printTimeAndThread("服务员收款 500元");
             SmallTool.sleepMillis(100);
             return "500";
-        }).thenApplyAsync(money -> {
+        });
+
+        CompletableFuture<String> invoice2 = invoice.thenApplyAsync(money -> {
             SmallTool.printTimeAndThread(String.format("服务员开发票 面额 %s元", money));
             SmallTool.sleepMillis(200);
-            return String.format("%s元发票", money);
-        });
+            return String.format("%s元发票", money);},executor);
+        CompletableFuture<String> invoice3 = invoice2.thenApplyAsync(money -> {
+            SmallTool.printTimeAndThread(String.format("服务员开发票 面额 %s元", money));
+            SmallTool.sleepMillis(200);
+            return String.format("%s元发票", money);},executor);
 
         SmallTool.printTimeAndThread("小白 接到朋友的电话，想一起打游戏");
 
-        SmallTool.printTimeAndThread(String.format("小白拿到%s，准备回家", invoice.join()));
+        SmallTool.printTimeAndThread(String.format("小白拿到%s，准备回家", invoice3.join()));
     }
 
 
--- meTest/src/test/java/com/me/test/controller/UserInfoControllerTest.java
+++ meTest/src/test/java/com/me/test/controller/UserInfoControllerTest.java
@@ -5,6 +5,8 @@ import com.me.test.Model.*;
 import org.junit.Test;
 
 import java.util.*;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ForkJoinPool;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
@@ -90,5 +92,37 @@ public class UserInfoControllerTest {
         });
         System.out.println(result.toString());
 
+    }
+
+    @Test
+    public void test5() throws Exception {
+        ForkJoinPool pool=new ForkJoinPool();
+        // 创建异步执行任务:
+        CompletableFuture<Double> cf = CompletableFuture.supplyAsync(()->{
+            System.out.println(Thread.currentThread()+" start job1,time->"+System.currentTimeMillis());
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+            }
+            System.out.println(Thread.currentThread()+" exit job1,time->"+System.currentTimeMillis());
+            return 1.2;
+        },pool);
+        //cf关联的异步任务的返回值作为方法入参，传入到thenApply的方法中
+        //thenApply这里实际创建了一个新的CompletableFuture实例
+        CompletableFuture<String> cf2=cf.thenApplyAsync((result)->{
+            System.out.println(Thread.currentThread()+" start job2,time->"+System.currentTimeMillis());
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+            }
+            System.out.println(Thread.currentThread()+" exit job2,time->"+System.currentTimeMillis());
+            return "test:"+result;
+        });
+        System.out.println("main thread start cf.get(),time->"+System.currentTimeMillis());
+        //等待子任务执行完成
+        System.out.println("run result->"+cf.get());
+        System.out.println("main thread start cf2.get(),time->"+System.currentTimeMillis());
+        System.out.println("run result->"+cf2.get());
+        System.out.println("main thread exit,time->"+System.currentTimeMillis());
     }
 }
