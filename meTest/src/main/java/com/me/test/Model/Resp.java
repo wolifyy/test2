6a3997d5e44112ff92b56be2a0598af1a58773fe
wolifyy <wolify@qq.com>
2022-12-11 23:30:19 +0800

    导入合并单元格

---
+++ meTest/src/main/java/com/me/test/Model/Resp.java
@@ -0,0 +1,15 @@
+package com.me.test.Model;
+
+import java.util.List;
+
+/**
+ * 人一能之,己百之；人十能之,己千之。果能此道矣,虽愚必明,虽柔必强。
+ *
+ * @author wolify
+ * @version 1.0
+ * @date 2022/11/27 21:55:39
+ */
+
+public class Resp<T> {
+    private List<T> pageLsit;
+}
